63db3c9c28be91d3501b4ec1dc4e0db031f90bc4
Justin Tooley <jstooley823@gmail.com>
2017-09-10 10:24:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,27 +21,28 @@ class Song
     @@genree.uniq!
   end
 
-  def genre_count
-    genre_hash ={}
-    @@genres.each do |genre|
-    if genre_hash.has_key?(genre)
-        genre_hash[genre] += 1
-    else
-        genre_hash[genre] = 1
-    end
-  end
+#  def genre_count
+#    genre_hash ={}
+#    @@genres.each do |genre|
+#    if genre_hash.has_key?(genre)
+#        genre_hash[genre] += 1
+#    else
+#        genre_hash[genre] = 1
+#    end
+#  end
 
   def artists
     @@artists.uniq!
   end
 
-  def artist_count
-    artist_hash = {}
-    @@artists.each do |artist|
-      if artist_hash.has_key?(artist)
-        artist_hash[artist] += 1
-      else
-        artist_hash[artist] = 1
-    end
+  #def artist_count
+  #  artist_hash = {}
+  #  @@artists.each do |artist|
+  #    if artist_hash.has_key?(artist)
+  #      artist_hash[artist] += 1
+  #    else
+  #      artist_hash[artist] = 1
+  #  end
   end
+
 end
