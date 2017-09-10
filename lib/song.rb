db6c45ebacccdb2257c231dadfe7d32cbda54f60
Justin Tooley <jstooley823@gmail.com>
2017-09-10 10:11:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  
+
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = []
@@ -36,7 +36,7 @@ class Song
   end
 
   def artist_count
-    artist_hash ={}
+    artist_hash = {}
     @@artists.each do |artist|
       if artist_hash.has_key?(artist)
         artist_hash[artist] += 1
